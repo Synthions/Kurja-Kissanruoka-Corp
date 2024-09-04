5c2354297e43d220faabe0f1ef3794ab2889b96e
iroguin <143116756+Iroguin@users.noreply.github.com>
2024-09-04 14:57:06 +0300

    fiddled with the kissanruoka

--- physics_platformer/kissanruoka.gd
+++ physics_platformer/kissanruoka.gd
@@ -1,20 +1,31 @@
 extends Node2D
 
+@onready var animation_player := $AnimationPlayer as AnimationPlayer
+
 var anim := ""
-
-enum State {
-	EXISTING,
-	DISSAPEARING,
-}
-
-var _State := State.EXISTING
+var pickedup := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	connect("area_entered", Callable(self, "_on_area_entered"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var new_anim := anim
+	if pickedup == false:
+		new_anim = "hover"
+#	else:
+#		new_anim = "dissapear"
+	
+	if anim != new_anim:
+		anim = new_anim
+		($AnimationPlayer as AnimationPlayer).play(anim)
 
+#func _on_area_entered(body: Node) -> void:
+#	if body is Player:
+#		dothething()
+#
+#func dothething():
+#	pickedup = true
+#	print("you have done the thing!")
